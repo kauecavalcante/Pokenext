3cab44bc31846cf31b2dc2e0ce88a95ad4660b3f
Kauê Cavalcante <102473287+kauecavalcante@users.noreply.github.com>
2022-08-01 19:19:00 -0300

    create Pokenext

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ["cdn.traction.one"],
+  },
+}
+
+
+/*
 /** @type {import('next').NextConfig} */
+/*
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ["cdn.traction.one"],
+  },
 }
 
 module.exports = nextConfig
+*/
+
