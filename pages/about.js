9e65893470de79ac104c6c0b20fc22fc0cb182a5
Kauê Cavalcante <102473287+kauecavalcante@users.noreply.github.com>
2022-08-01 20:54:07 -0300

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -7,9 +7,9 @@ export default function About() {
         <div className={styles.about}>
             <h1>Sobre o Projeto</h1>
             <p> Pokenext é um app construído em Next.js para consultar Pokémons</p>
-            <p>Feito pelo <a href="https://github.com/kauecavalcante" target="_blank"><span className={styles.name}>Kauê Cavalcante</span></a> <br/>
-                API usada <a className={styles.name} href="https://pokeapi.co/" target="_blank">pokeapi</a> <br/>
-                projeto no <a className={styles.name} href="https://github.com/kauecavalcante/Pokenext" target="_blank">github</a>
+            <p>Feito pelo <a href="https://github.com/kauecavalcante" ><span className={styles.name}>Kauê Cavalcante</span></a> <br/>
+                API usada <a className={styles.name} href="https://pokeapi.co/">pokeapi</a> <br/>
+                projeto no <a className={styles.name} href="https://github.com/kauecavalcante/Pokenext" >github</a>
             </p>
             <Image src="/images/charizard.png" width="300" height="300" alt="Charizard" />
         </div>
